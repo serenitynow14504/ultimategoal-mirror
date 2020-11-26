6b3b56addff9a2c34218a1df04e147e0453f14ae
Chinmay Murthy <chinmaymurthy474@gmail.com>
2020-11-26 14:31:22 -0800

    intake jam detection and correction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/Intake.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.RobotComponents;
 
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.ExpansionHubMotor;
 
 class Intake extends Capability {
     private ExpansionHubMotor intakeWheels;
+    private DistanceSensor stuckSensor;
     private final double CURRENT_THRESHOLD = 10;
+    private ElapsedTime stuckTime;
 
     public Intake(Robot parent) {
         super(parent);
@@ -16,6 +21,8 @@ class Intake extends Capability {
 
     public void init(HardwareMap hardwareMap) {
         intakeWheels = (ExpansionHubMotor)hardwareMap.get("intake");
+        stuckSensor = hardwareMap.get(DistanceSensor.class, "ramp");
+        stuckTime = new ElapsedTime();
     }
 
     public void teleOp(Gamepad gamepad) {
@@ -35,7 +42,7 @@ class Intake extends Capability {
     }
 
     public void reverse() {
-        intakeWheels.setPower(-1);
+        intakeWheels.setPower(-0.5);
     }
 
     public void off() {
@@ -50,9 +57,36 @@ class Intake extends Capability {
         }
     }
 
+    public boolean ringStuck() {
+        return sensorDist() < 15;
+    }
+
+    public double sensorDist() {
+        return stuckSensor.getDistance(DistanceUnit.MM);
+    }
+
+    private void reTakeRing() {
+        off();
+        sleep(200);
+        while(ringStuck()) {
+            reverse();
+        }
+        off();
+        sleep(100);
+        on();
+    }
+
     @Override
     public void run() {
+        stuckTime.reset();
         while(opModeIsActive()) {
+            if(ringStuck()) {
+                if(stuckTime.milliseconds()>500) {
+                    reTakeRing();
+                }
+            } else {
+                stuckTime.reset();
+            }
             if (intakeWheels.getCurrentDraw(ExpansionHubEx.CurrentDrawUnits.AMPS) > CURRENT_THRESHOLD) {
                 intakeWheels.setPower(0);
                 parent.setLedColors(255, 0, 0);
