942ad023a95ae747944f84d832e1bb394fa7b07d
Chinmay Murthy <chinmaymurthy474@gmail.com>
2020-12-18 20:28:31 -0800

    added splines for real

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/PathPlanning/Paths/LineSegment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/PathPlanning/Paths/LineSegment.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.RobotComponents.PathPlanning.Paths;
 
-class LineSegment implements Segment {
+import org.firstinspires.ftc.teamcode.Common.VectorD;
+
+class LineSegment extends PolynomialSegment {
+    public LineSegment(VectorD start, VectorD end) {
+        super(start, end.subtracted(start), end, end.subtracted(start));
+    }
 }
