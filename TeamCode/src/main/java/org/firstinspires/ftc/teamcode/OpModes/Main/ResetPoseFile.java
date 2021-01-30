71b7ab16f6bb07e2de5ef2b5289c08aad429eadf
Chinmay Murthy <chinmaymurthy474@gmail.com>
2021-01-29 20:50:32 -0800

    fixed lift init, tested new features except actual shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main/ResetPoseFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main/ResetPoseFile.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.OpModes.Main;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Common.Utilities;
 import org.firstinspires.ftc.teamcode.RobotComponents.Constants.FieldConstants;
 import org.firstinspires.ftc.teamcode.RobotComponents.Constants.RobotConstants;
 import org.firstinspires.ftc.teamcode.RobotComponents.Robot;
@@ -14,6 +15,11 @@ public class ResetPoseFile extends LinearOpMode {
     public void runOpMode() {
         robot = new Robot(this, RobotConstants.ALLIANCES.SOLO, FieldConstants.EMPTY_FIELD, 0,
                 0, 0, true);
+        try {
+            Utilities.log(robot.readPose().toString());
+        } catch (Exception e) {Utilities.log("no pose to read");}
         robot.writePose();
+
+        waitForStart();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/Lift.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.RobotComponents;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -9,22 +10,26 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Common.Line;
 import org.firstinspires.ftc.teamcode.Common.PIDController;
+import org.firstinspires.ftc.teamcode.Common.Utilities;
 import org.firstinspires.ftc.teamcode.Common.VectorD;
 import org.openftc.revextensions2.ExpansionHubMotor;
 
 public class Lift extends Capability {
+    private HardwareMap hardwareMap;
     private ExpansionHubMotor platform;
     private Rev2mDistanceSensor platformSensor;
     private final int POSITION_DIFFERENCE = 6750, liftPosThresh = 60;//8924
     private int bottomPos, topPos;
     private double targetLiftPos;
 
-    private final double PULLEY_DIAMETER = 25;//mm
-    private final int CLICKS_PER_REV = 2048;
-    private final double MM_PER_CLICK = CLICKS_PER_REV / (2. * Math.PI * PULLEY_DIAMETER);
+    private boolean sensorDied = false;
+
+    private final double PULLEY_DIAMETER = 30;//mm
+    private final int CLICKS_PER_REV = 8192;//2048
+    private final double MM_PER_CLICK = (Math.PI * PULLEY_DIAMETER) / CLICKS_PER_REV;
 
 
-    public static double LiftKP = 0.0075;
+    public static double LiftKP = 0.00675;
     public static double LiftKI = 0;
     public static double LiftKD = 0.0015;
 
@@ -45,8 +50,10 @@ public class Lift extends Capability {
     void init(HardwareMap hardwareMap) {
         platform = (ExpansionHubMotor)hardwareMap.get("platform");
         platform.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        platform.setDirection(DcMotorSimple.Direction.REVERSE);
         platformSensor = (Rev2mDistanceSensor)hardwareMap.get(DistanceSensor.class, "liftHeight");
 
+        this.hardwareMap = hardwareMap;
         topPos = platform.getCurrentPosition();
         bottomPos = topPos - POSITION_DIFFERENCE;
 
@@ -54,7 +61,8 @@ public class Lift extends Capability {
 
         do {
             initialDist = platformSensor.getDistance(DistanceUnit.MM);
-        } while(initialDist>LiftTopPosDist+100);
+            Utilities.log("sensor: " + initialDist);
+        } while(!validDist(initialDist));
         encoderToDist = new Line(new VectorD(platform.getCurrentPosition(), initialDist), MM_PER_CLICK);
 
         liftPID = new PIDController(LiftKP, LiftKI, LiftKD);
@@ -64,13 +72,15 @@ public class Lift extends Capability {
     }
 
     public void teleOp(Gamepad gamepad1, Gamepad gamepad2) {
+        /*telemetry.addData("lift safe dist: ", getLiftHeightMM());
         telemetry.addData("lift dist: ", platformSensor.getDistance(DistanceUnit.MM));
-        telemetry.addLine();
+        telemetry.addData("lift encoder 'dist': ", encoderToDist.yFromX(platform.getCurrentPosition()));
         telemetry.addData("lift pos: ", platform.getCurrentPosition());
         telemetry.addData("lift target pos: ", targetLiftPos);
         telemetry.addData("lift power: ", PIDPow);
+        telemetry.addData("g2 left stick y: ", gamepad2.left_stick_y);
+        telemetry.update();*/
 
-        telemetry.update();
         if(gamepad2.left_stick_y<-0.5) {
             liftUp();
         }
@@ -79,10 +89,31 @@ public class Lift extends Capability {
         }
     }
 
+    public void debugOp(Gamepad gamepad1, Gamepad gamepad2) {
+        telemetry.addData("lift safe dist: ", getLiftHeightMM());
+        telemetry.addData("lift dist: ", platformSensor.getDistance(DistanceUnit.MM));
+        telemetry.addData("lift encoder 'dist': ", encoderToDist.yFromX(platform.getCurrentPosition()));
+        telemetry.addData("lift pos: ", platform.getCurrentPosition());
+        telemetry.addData("lift target pos: ", targetLiftPos);
+        telemetry.addData("lift power: ", PIDPow);
+        telemetry.addData("g2 left stick y: ", gamepad2.left_stick_y);
+        telemetry.update();
+
+        liftPID.disable();
+        PIDPow = 0;
+        platform.setPower(gamepad2.left_stick_y/3);
+    }
+
     private double getLiftHeightMM() {
         double distance = platformSensor.getDistance(DistanceUnit.MM);
-        if(distance<LiftTopPosDist+100) return distance;
+        if(validDist(distance)) return distance;
+        sensorDied = true;
+        parent.setLedColors(255, 0, 0);
         return encoderToDist.yFromX(platform.getCurrentPosition());
+    }
+
+    private boolean validDist(double d) {
+        return (d<LiftTopPosDist+20 && d>0);
     }
 
 
@@ -97,9 +128,17 @@ public class Lift extends Capability {
     @Override
     public void run() {
         while(opModeIsActive()) {
-            liftPID.setSetpoint(targetLiftPos);
-            PIDPow = liftPID.performPID(getLiftHeightMM());
+            if(liftPID.isEnabled()) {
+                liftPID.setSetpoint(targetLiftPos);
+                PIDPow = liftPID.performPID(getLiftHeightMM());
+            }
             platform.setPower(PIDPow);
+
+            if(sensorDied && Math.abs(PIDPow)<0.05) {
+                platformSensor = (Rev2mDistanceSensor)hardwareMap.get(DistanceSensor.class, "liftHeight");
+                sensorDied = false;
+                parent.setLedColors(0, 255, 0);
+            }
         }
     }
 }
